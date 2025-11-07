4fdae8958ac754cc446589e209a73e5094d8a4ae
suhyeon <tngutnqls6909@gmail.com>
2025-11-07 20:28:34 +0900

    Feat: 게시글 API 추가
    전체 게시글 조회, 게시글 수정, 삭제 API 추가 구현

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -4,12 +4,14 @@ package com.example.board.controller;
 import com.example.board.dto.PostCreateRequest;
 import com.example.board.dto.PostResponse;
 import com.example.board.service.PostService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,12 +32,42 @@ public class PostController {
     }
 
     /**
-     * 게시물 조회
+     * 게시물 조회 (단일)
      */
     @GetMapping("/{id}")
     public ResponseEntity<PostResponse> getPostById(@PathVariable Long id) {
-        PostResponse response = postService.getPostById(id);
+        PostResponse post = postService.getPostById(id);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(post);
+    }
+
+    /**
+     * 게시물 조회 (리스트)
+     */
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        List<PostResponse> allPosts = postService.getAllPosts();
+
+        return ResponseEntity.ok(allPosts);
+    }
+
+    /**
+     * 게시물 수정
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponse> updatePost(@PathVariable Long id, @RequestBody PostCreateRequest request) {
+        PostResponse postResponse = postService.updatePost(id, request);
+
+        return ResponseEntity.ok(postResponse);
+    }
+
+    /**
+     * 게시물 삭제
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/board/service/PostService.java
+++ src/main/java/com/example/board/service/PostService.java
@@ -8,6 +8,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,7 +38,7 @@ public class PostService {
     }
 
     /**
-     * 게시물 조회
+     * 게시물 조회 (단일)
      */
     public PostResponse getPostById(Long id) {
         // 엔티티 조회
@@ -45,5 +47,42 @@ public class PostService {
         );
 
         return new PostResponse(post);
+    }
+
+    /**
+     * 게시물 조회 (전체)
+     */
+    public List<PostResponse> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+
+        return posts.stream().map(PostResponse::new).toList();
+    }
+
+    /**
+     * 게시물 수정
+     */
+    @Transactional
+    public PostResponse updatePost(Long id, PostCreateRequest request) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시물이 존재하지 않습니다.")
+        );
+
+        //  JPA의 변경 감지 기능으로 자동으로 저장됨 (save 안 해도 됨)
+        post.setTitle(request.getTitle());
+        post.setContent(request.getContent());
+
+        return new PostResponse(post);
+    }
+
+    /**
+     * 게시물 삭제
+     */
+    @Transactional
+    public void deletePost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시물이 존재하지 않습니다.")
+        );
+
+        postRepository.delete(post);
     }
 }
